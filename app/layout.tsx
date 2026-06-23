7ba6ffd96bfd7aa046b5baa4d74182f6e4418c33
emb91 <emmabardsley91@gmail.com>
2026-06-23 23:09:40 +1200

    Add Apollo visitor tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -101,6 +101,14 @@ export default function RootLayout({
           gtag('config', 'G-0WTVF1D48X');
         ` }} />
 
+        {/* Apollo website tracker (company + person-level visitor identification) */}
+        <script dangerouslySetInnerHTML={{ __html: `
+          function initApollo(){var n=Math.random().toString(36).substring(7),o=document.createElement("script");
+          o.src="https://assets.apollo.io/micro/website-tracker/tracker.iife.js?nocache="+n,o.async=!0,o.defer=!0,
+          o.onload=function(){window.trackingFunctions.onLoad({appId:"689866f9353c4d0015f32da8"})},
+          document.head.appendChild(o)}initApollo();
+        ` }} />
+
         {/* Organization Schema Markup */}
         <script
           type="application/ld+json"
